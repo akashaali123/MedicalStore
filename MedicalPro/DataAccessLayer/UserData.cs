cb7461642438a016937c11cb818faa6906e828a8
akashaali123 <akashaali2012@gmail.com>
2020-11-03 10:19:19 +0500

    Add User data Access Layer

---
+++ MedicalPro/DataAccessLayer/UserData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalPro.DataAccessLayer
+{
+    public class UserData
+    {
+        private readonly MedicalProEntities _context;
+        public static int UserId { get; set; }
+        public UserData()
+        {
+            _context = new MedicalProEntities();
+        }
+
+        public  bool ValidateUser(string name,string password)
+        {
+            try
+            {
+                var user = _context.tblUsers.Where(x => x.UserName == name && x.Password == password && x.IsActive == true)
+                                   .FirstOrDefault();
+                UserId = user.UserId;
+                if (user != null)
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+         
+        }
+       
+
+       
+    }
+}
